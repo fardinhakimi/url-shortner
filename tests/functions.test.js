4a87d70153367d68528e8b4a9f49375506ac6546
fardinhakimi <fardinhakimi@gmail.com>
2019-07-15 16:27:00 +0200

    switching to mocha, chai, sinon as it is more suited for testing server based apps rather than jest which is better for testing react apps

--- tests/functions.test.js
+++ tests/functions.test.js
@@ -1,36 +1,37 @@
 
 
 const { isEmpty } = require('../functions')
+const { expect } = require('chai')
 
 describe('Testing isEmpty function', () => {
 
     it('tests that true is not empty', () => {
 
-        expect(isEmpty(true)).toBe(false);
+        expect(isEmpty(true)).to.eql(false)
     })
 
     it('tests that empty string is empty', () => {
-        expect(isEmpty('')).toBe(true);
+        expect(isEmpty('')).to.eql(true)
     })
 
     it('tests that false is empty', () => {
-        expect(isEmpty(false)).toBe(true)
+        expect(isEmpty(false)).to.eql(true)
     })
 
     it('tests that null is empty', () => {
-        expect(isEmpty(null)).toBe(true)
+        expect(isEmpty(null)).to.eql(true)
     })
 
     it('tests that undefined is empty', () => {
-        expect(isEmpty(undefined)).toBe(true)
+        expect(isEmpty(undefined)).to.eql(true)
     })
 
     it('tests that 5 is not empty', () => {
-        expect(isEmpty(5)).toBe(false)
+        expect(isEmpty(5)).to.eql(false)
     })
 
     it('tests that "lazy dog" is not empty', () => {
-        expect(isEmpty('lazy dog')).toBe(false)
+        expect(isEmpty('lazy dog')).to.eql(false)
     })
 })
 
